5c7527748ad654de49e0c1791c2645853c25c4dc
Alexander Stukov <a.stukov.dev@gmail.com>
2026-01-14 15:50:21 +0300

    Update keymap.

--- lua/luminalis/lazy/neo-tree.lua
+++ lua/luminalis/lazy/neo-tree.lua
@@ -1,12 +1,15 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        },
+        lazy = false, -- neo-tree will lazily load itself
+        config = function()
+            vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = 'Toggle file manager' })
+        end
+    }
 }
